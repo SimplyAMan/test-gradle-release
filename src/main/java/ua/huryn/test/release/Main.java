9c9afa3970b9b556edce4ba118b8a558a5121fc8
simplyaman <simplyaman@gmail.com>
2020-12-05 09:58:33 +0200

    commit in first release branch

--- src/main/java/ua/huryn/test/release/Main.java
+++ src/main/java/ua/huryn/test/release/Main.java
@@ -3,5 +3,6 @@ package ua.huryn.test.release;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("First_release");
     }
 }
