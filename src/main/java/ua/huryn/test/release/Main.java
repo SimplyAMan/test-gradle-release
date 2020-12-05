40526d48457d1df116ebee4a27fc88b435e1be1a
simplyaman <simplyaman@gmail.com>
2020-12-05 09:53:34 +0200

    commit in first branch

--- src/main/java/ua/huryn/test/release/Main.java
+++ src/main/java/ua/huryn/test/release/Main.java
@@ -3,5 +3,6 @@ package ua.huryn.test.release;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("it's first branch. In master we have tag 0.1.0 and 0.1.1 in gradle.properties ");
     }
 }
